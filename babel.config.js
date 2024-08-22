569ed8050a058864c2c14b4de72092478d68e621
ElianThe <guiffaul2u@etu.univ-lorraine.fr>
2024-08-22 12:29:03 +0200

    modification de quelques fichiers de configuration

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,20 @@
+// fichier de configuration pour convertir du code js/ts en une version compatible
+// avec les environnements js + anciens, tels que des navigateurs + anciens
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ['module:react-native-dotenv', {
-        envName: 'APP_ENV',
-        moduleName: '@env',
-        path: '.env',
-        whitelist: ['API_URL', 'TOKEN_KEY'],
-        safe: false,
-        allowUndefined: true,
-      }],
-    ]
-  };
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'], // ensemble de configurations de plugins prédéfinis qui aident Babel à transformer le code
+        plugins: [ // liste les plugins Babel utilisés pour ajouter ou personnaliser des fonctionnalités spécifiques lors de la compilation du code
+            ['module:react-native-dotenv', // permet d'importer des variables d'environnement définies dans un fichier .env
+                {
+                    envName: 'APP_ENV',
+                    moduleName: '@env',
+                    path: '.env',
+                    whitelist: ['API_URL', 'TOKEN_KEY'],
+                    safe: false,
+                    allowUndefined: true,
+                }
+            ],
+        ]
+    };
 };
