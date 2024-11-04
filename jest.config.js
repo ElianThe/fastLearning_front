ad0fe80ab61314e5853db312002671bdaa1433ca
ElianThe <guiffaul2u@etu.univ-lorraine.fr>
2024-11-04 15:43:41 +0100

    configuration du fichier jest pour utiliser le coverage

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,15 @@ module.exports = {
         '^@/(.*)$': '<rootDir>/$1',
     },
     testPathIgnorePatterns: [
-        '<rootDir>/node_modules/'
+        '<rootDir>/node_modules/',
         '<rootDir>/build/',
+    ],
+    collectCoverageFrom: [
+        '**/*.{js,jsx,ts,tsx}', // couvre tous les fichiers JavaScript/TypeScript dans le répertoire principal
+        '!node_modules/**', // exclut le dossier node_modules
+        '!coverage/**',
+        '!**/_layout.tsx',
+        '!**/.*.js',
+        '!**/*.config.js',
     ],
 };
