3b969598c76a38a6b2bdb11aacdd6c536b831482
ElianThe <guiffaul2u@etu.univ-lorraine.fr>
2024-12-01 11:35:35 +0100

    ajout de jest dans le lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-    extends: ["expo", "prettier"],
-    plugins: ["prettier"],
+    extends: ["expo", "prettier", "plugin:jest/recommended"],
+    plugins: ["prettier", "jest"],
     rules: {
         "prettier/prettier": ["warn"],
         "import/no-unresolved": "off", // Pour éviter les erreurs sur les imports non résolus comme `@env`
